6a4944df2120a3f6dc303239b311d23d07f0885b
Daisuke Baba <baba.daisuke@gmail.com>
2016-12-01 14:41:05 +0900

    Use topic when msg object is available

--- src/sequence-functions.es6.js
+++ src/sequence-functions.es6.js
@@ -40,6 +40,7 @@ export default function(RED) {
         appendEventSequnce(msg);
         if (this.onNewMessage || ('capture' in msg)) {
           this.send({
+            topic: msg.topic,
             payload: copy()
           });
         }
